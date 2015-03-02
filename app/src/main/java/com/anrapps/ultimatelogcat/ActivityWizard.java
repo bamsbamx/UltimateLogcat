1fa7ed5268d5f455e10e52a7f1d541322c824ce2
Aner <anernaiz@gmail.com>
2015-03-02 17:46:12 +0100

    -Creating ActivityWizard (II) finished

--- app/src/main/java/com/anrapps/ultimatelogcat/ActivityWizard.java
+++ app/src/main/java/com/anrapps/ultimatelogcat/ActivityWizard.java
@@ -1,53 +1,69 @@
 package com.anrapps.ultimatelogcat;
 
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.os.PersistableBundle;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.widget.Toast;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.view.View;
-import com.anrapps.ultimatelogcat.util.PrefUtils;
-import android.app.FragmentTransaction;
-import android.app.FragmentManager;
 import android.app.Fragment;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
-import android.animation.Animator;
-import android.view.animation.DecelerateInterpolator;
-import android.view.ViewAnimationUtils;
-import android.util.Log;
-import com.anrapps.ultimatelogcat.widget.AutoTextView;
+import android.widget.Toast;
+
+import com.anrapps.ultimatelogcat.util.PrefUtils;
+import com.stericson.RootShell.RootShell;
+import com.stericson.RootShell.exceptions.RootDeniedException;
+import com.stericson.RootShell.execution.Command;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class ActivityWizard extends ActionBarActivity implements OnClickListener {
 
-	private Button buttonSu, buttonAdb;
-	
-	//TODO: Hide fragment (if attached) onbackpressed
-	
-	@Override
+    private ViewPager mViewPager;
+
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		overridePendingTransition(0, 0);
 		setContentView(R.layout.activity_wizard);
-		
-		buttonSu = (Button) findViewById(R.id.activitywizardButton2);
-		buttonAdb = (Button) findViewById(R.id.activitywizardButton3);
-		buttonSu.setOnClickListener(this);
-		buttonAdb.setOnClickListener(this);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_wizard_viewpager);
+        mViewPager.setOffscreenPageLimit(0);
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(getFragmentManager()) {
+            @Override public Fragment getItem(int position) {
+                return FragmentWizard.newInstance(position);
+            }
+            @Override public int getCount() {
+                return 2;
+            }
+        });
 	}
-	
-	@Override
+
+    @Override
+    public void onBackPressed() {
+        if (mViewPager.getCurrentItem() == 0)
+            super.onBackPressed();
+        else mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
+    }
+
+    @Override
 	public void onClick(View view) {
 		switch (view.getId()) {
-			case R.id.activitywizardButton2:
+			case R.id.fragment_wizard_intro_root_button:
 				getPermissionViaRoot();
 				break;
-			case R.id.activitywizardButton3:
-				showAdbFragment();
+			case R.id.fragment_wizard_intro_adb_button:
+				mViewPager.setCurrentItem(1);
 				break;
+            case R.id.fragment_wizard_done_button:
+                PrefUtils.setWizardDone(this, true);
+                ActivityMain.start(this, true);
+                break;
 		}
 	}
 	
@@ -57,65 +73,69 @@ public class ActivityWizard extends ActionBarActivity implements OnClickListener
 	}
 	
 	private void getPermissionViaRoot() {
-		//TODO
-	}
-	
-	private void showAdbFragment() {
-		int[] coords = new int[2];
-		buttonAdb.getLocationOnScreen(coords);
-		int buttonX = (buttonAdb.getRight() - buttonAdb.getLeft())/2;
-		int buttonY = (buttonAdb.getBottom() - buttonAdb.getTop())/2;
-		FragmentWizard f = FragmentWizard.newInstance(
-				coords[0]+buttonX, 
-				coords[1]+buttonY);
-		getFragmentManager().beginTransaction()
-				.replace(R.id.activity_wizard_fragment_container, f)
-				.commit();
-	}
-	
+        Command command = new Command(0, ("pm grant " + getPackageName() + " android.permission.READ_LOGS | echo \"logcat\"")) {
+            private boolean success = true;
+            @Override public void commandOutput(int id, String line) {
+                if (line.toLowerCase().contains("exception")) success = false;
+                super.commandOutput(id, line);
+            }
+            @Override public void commandTerminated(int id, String reason) {
+                Toast.makeText(ActivityWizard.this, R.string.text_root_error, Toast.LENGTH_LONG).show();
+            }
+            @Override public void commandCompleted(int id, int exitCode) {
+                Toast.makeText(ActivityWizard.this, success ? "success" : "error", Toast.LENGTH_SHORT).show();
+                if (success) {
+                    PrefUtils.setWizardDone(ActivityWizard.this, true);
+                    ActivityMain.start(ActivityWizard.this, true);
+                }
+                else Toast.makeText(ActivityWizard.this, R.string.text_root_error, Toast.LENGTH_LONG).show();
+            }
+        };
+        try {
+            RootShell.getShell(true).add(command);
+        } catch (IOException | RootDeniedException | TimeoutException e) {
+            Toast.makeText(ActivityWizard.this, R.string.text_root_error, Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+
 	public static class FragmentWizard extends Fragment {
 
-		public static FragmentWizard newInstance(int centerX, int centerY) {
+        private static final String BUNDLE_KEY_FRAGMENT_POSITION = "fragment_position";
+
+        private Button buttonSu, buttonAdb, buttonDone;
+
+        public static FragmentWizard newInstance(int position) {
 			Bundle args = new Bundle();
-			args.putInt("cx", centerX);
-			args.putInt("cy", centerY);
+			args.putInt(BUNDLE_KEY_FRAGMENT_POSITION, position);
 			FragmentWizard fragment = new FragmentWizard();
 			fragment.setArguments(args);
 			return fragment;
-
 		}
+
+        public FragmentWizard() {}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_wizard, container, false);
+            final int position = getArguments().getInt(BUNDLE_KEY_FRAGMENT_POSITION);
+            final int layoutIdToInflate = position == 0 ?
+                    R.layout.fragment_wizard_intro : R.layout.fragment_wizard_adb;
+            View rootView = inflater.inflate(layoutIdToInflate, container, false);
 
-			rootView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-					@Override
-					public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-						v.removeOnLayoutChangeListener(this);
-						int cx = getArguments().getInt("cx");
-						int cy = getArguments().getInt("cy");
-						int radius = (int)Math.hypot(right, bottom);
-
-						Animator reveal = ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, radius);
-						reveal.setInterpolator(new DecelerateInterpolator(2f));
-						reveal.setDuration(1000);
-						reveal.start();
-					}
-				});
-				//TODO:Change IDs
-			Button doneButton = (Button) rootView.findViewById(R.id.fragmentwizardButton1);
-			doneButton.setOnClickListener(new OnClickListener() {
-				@Override public void onClick(View v) {
-					ActivityMain.start(getActivity(), true);
-					//TODO Set pref: PrefUtils.setWizardDone(getActivity(), true);
-				}
-			});
-				
-			AutoTextView autoTextView = (AutoTextView) rootView.findViewById(R.id.fragment_wizard_autotextview);
-			autoTextView.setAutoText(R.string.text_adb_command);
-
-			return rootView;
-		}		
-	}
+            if (position == 0) {
+                buttonSu = (Button) rootView.findViewById(R.id.fragment_wizard_intro_root_button);
+		        buttonAdb = (Button) rootView.findViewById(R.id.fragment_wizard_intro_adb_button);
+                if (getActivity() instanceof OnClickListener) {
+                    buttonSu.setOnClickListener((OnClickListener) getActivity());
+                    buttonAdb.setOnClickListener((OnClickListener) getActivity());
+                }
+            } else if (position == 1) {
+                buttonDone = (Button) rootView.findViewById(R.id.fragment_wizard_done_button);
+                if (getActivity() instanceof OnClickListener)
+                    buttonDone.setOnClickListener((OnClickListener) getActivity());
+            }
+            return rootView;
+        }
+    }
 }
---
+++ app/src/main/java/com/anrapps/ultimatelogcat/widget/DisabledViewPager.java
@@ -0,0 +1,28 @@
+package com.anrapps.ultimatelogcat.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class DisabledViewPager extends ViewPager {
+
+
+    public DisabledViewPager(Context context) {
+        super(context);
+    }
+
+    public DisabledViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
